029ba40b419cce2e791a8e23d8c16147a7995271
Rohit Soni <Rsony.721@gmail.com>
2021-12-28 06:04:44 +0530

    word generator app in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import arrayOfwords from '../../utils/words';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words: string = '';
+  limit: any = 10;
+
+  handleSlideChange(newLimit: number) {
+    this.limit = newLimit;
+  }
+
+  generate() {
+    this.words = arrayOfwords.slice(0, this.limit).join(' ');
+  }
 }
